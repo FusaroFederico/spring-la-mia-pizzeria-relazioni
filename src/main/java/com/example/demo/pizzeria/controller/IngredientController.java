18364408ea5f0c74b03cd37ffb8b80a1c7f4e2e4
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-09-20 18:01:44 +0200

    Update IngredientController.java
    Add methods that manage routes for edit and delete crud

--- src/main/java/com/example/demo/pizzeria/controller/IngredientController.java
+++ src/main/java/com/example/demo/pizzeria/controller/IngredientController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -27,7 +28,7 @@ public class IngredientController {
 	
 	@GetMapping
 	public String index(Model model) {
-		model.addAttribute("title", "Lista Ingredienti");
+		model.addAttribute("title", "Ingredienti");
 		model.addAttribute("ingredients", ingredientService.findAllSortedByName());
 		
 		return "/ingredients/index";
@@ -37,7 +38,7 @@ public class IngredientController {
 	@GetMapping("/create")
 	public String create(Model model) {
 		model.addAttribute("title", "Aggiungi Ingrediente");
-		model.addAttribute("pizzas", pizzaService.findAllSortedByName());
+		model.addAttribute("pizzasList", pizzaService.findAllSortedByName());
 		model.addAttribute("ingredient", new Ingredient());
 		
 		return "/ingredients/create";
@@ -46,16 +47,64 @@ public class IngredientController {
 	@PostMapping ("/create")
 	public String store(@Valid @ModelAttribute("ingredient") Ingredient formIngredient, 
 					     BindingResult bindingResult,
+					     Model model,
 					     RedirectAttributes redirectAttributes) {
 		
 		if (bindingResult.hasErrors()) {
+			model.addAttribute("pizzasList", pizzaService.findAllSortedByName());
 			return "/ingredients/create";
 		}
 		
 		ingredientService.create(formIngredient);
-		
+	
 		redirectAttributes.addFlashAttribute("successMessage", "L'Ingrediente '" + formIngredient.getName() + "' è stato aggiunto alla lista.");
 		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+		return "redirect:/ingredients";
+	}
+	
+	// EDIT
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable("id") Integer id, Model model) {
+		
+		model.addAttribute("title", "Modifica Ingrediente");
+		model.addAttribute("ingredient", ingredientService.getById(id));
+		model.addAttribute("pizzasList", pizzaService.findAllSortedByName());
+		
+		return "/ingredients/edit";
+	}
+	
+	@PostMapping("/edit/{id}")
+	public String update(@Valid @ModelAttribute("ingredient") Ingredient ingredientToUpdate, 
+						 Model model,
+						 BindingResult bindingResult,
+						 RedirectAttributes redirectAttributes) {
+		
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("pizzasList", pizzaService.findAllSortedByName());
+			return "/ingredients/edit";
+		}
+		
+		ingredientService.update(ingredientToUpdate);
+		
+		redirectAttributes.addFlashAttribute("successMessage", "L'ingrediente '" + ingredientToUpdate.getName() + "' è stato aggiornato con successo.");
+		redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+		return "redirect:/ingredients";
+	}
+	
+	// DELETE
+	// post -> delete
+	@PostMapping("/delete/{id}")
+	public String delete(@PathVariable("id") Integer id, RedirectAttributes redirectAttributes) {
+		
+		String ingredientName = ingredientService.getById(id).getName();
+		
+		// remove pizzas association
+		//ingredientService.getById(id).removePizzaAssociation();
+		
+		ingredientService.deleteById(id);
+		
+		redirectAttributes.addFlashAttribute("successMessage", "L'ingrediente '" + ingredientName + "' è stato eliminato con successo.");
+		redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
 		return "redirect:/ingredients";
 	}
 }
